e05b484762a23e0587c27100b09c7ad0a7936782
Roberto Natella <roberto.natella@unina.it>
2020-10-22 18:39:14 +0200

    Esempi script while e command substitution

---
+++ shell_scripting/esempi_scripting/esempio-command-sub.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+LINEE=$(cat $0 | wc -l)
+
+echo "Questo file ha $LINEE linee."
+
---
+++ shell_scripting/esempi_scripting/esempio-while.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+ls | while read LINEA
+do
+	if [ -f "$LINEA" ]
+	then
+		echo "$LINEA è un file"
+	fi
+
+	if [ -d "$LINEA" ]
+	then
+		echo "$LINEA è una cartella"
+	fi
+done
+
