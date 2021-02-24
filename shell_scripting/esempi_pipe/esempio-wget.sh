a28bdea5c8da645e80f22af82c3f1648c235406d
Roberto Natella <2117196+rnatella@users.noreply.github.com>
2021-02-24 11:22:12 +0100

    Esempio pipe con cut

--- shell_scripting/esempi_pipe/esempio-wget.sh
+++ shell_scripting/esempi_pipe/esempio-wget.sh
@@ -2,5 +2,5 @@
 
 wget https://github.com/acaudwell/Logstalgia/raw/master/data/example.log
 
-cat example.log | awk '{print $1}' | sort | uniq -c | sort -rn | head -3
+cat example.log | cut -d ' ' -f 1 | sort | uniq -c | sort -rn | head -3
 
