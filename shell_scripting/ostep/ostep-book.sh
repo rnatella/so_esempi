0b3a4857720dda25639d7cf7a236180731693645
Roberto Natella <2117196+rnatella@users.noreply.github.com>
2021-10-19 19:47:12 +0200

    Fix script OSTEP book

--- shell_scripting/ostep/ostep-book.sh
+++ shell_scripting/ostep/ostep-book.sh
@@ -7,7 +7,9 @@ awk -F, '{print $3}' list.txt | while read PDF
 do 
 	I=$(($I+1))
 
-	wget -O "OSTEP-$I-$PDF"  "http://pages.cs.wisc.edu/~remzi/OSTEP/$PDF"
+	PDF_FILENAME=$(printf 'OSTEP-%02d-%s' $I $PDF)
+
+	wget -O $PDF_FILENAME  "http://pages.cs.wisc.edu/~remzi/OSTEP/$PDF"
 done
 
 pdftk OSTEP-*.pdf cat output output.pdf
@@ -16,6 +18,7 @@ export PAGE=1
 
 ls -v OSTEP-*.pdf | while read PDF
 do
+
 	HEAD=`pdftotext $PDF -| head -2 |sed 'N;s/\n/ /'`
 	NUM=`pdftk $PDF dump_data | grep NumberOfPages | awk '{print $2}' | tr -dc '[0-9]'`
 	echo "$HEAD/$PAGE"
