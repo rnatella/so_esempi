989496d5d8ba86ee82579880bfbb90d5ca67b14a
Roberto Natella <2117196+rnatella@users.noreply.github.com>
2022-08-18 15:45:10 +0200

    Powershell

--- virtual-machine/vm-setup.sh
+++ virtual-machine/vm-setup.sh
@@ -109,6 +109,14 @@ su - so -c 'code --install-extension ms-vscode.cpptools'
 usermod -a -G docker so
 
 
+# Install PowerShell
+apt-get install -y wget apt-transport-https software-properties-common
+wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
+dpkg -i packages-microsoft-prod.deb
+rm -f packages-microsoft-prod.deb
+apt-get update
+apt-get install -y powershell
+
 
 # Install QEMU and virt-manager
 apt-get install -y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager
