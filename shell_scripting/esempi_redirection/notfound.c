ed6b20b6e5a6358c2f0076a1885a3b1e8eebd5fc
Roberto Natella <2117196+rnatella@users.noreply.github.com>
2022-11-09 09:31:38 +0100

    Esempio perror

--- shell_scripting/esempi_redirection/notfound.c
+++ shell_scripting/esempi_redirection/notfound.c
@@ -7,7 +7,7 @@ int main() {
  int fd = open("/home/file_che_non_esiste.txt", O_RDONLY);
 
  if(fd < 0) {
-    fprintf(stderr, "Non riesco ad aprire il file!\n");
+    perror("Non riesco ad aprire il file");
     exit(1);
  }
 
