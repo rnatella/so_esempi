6270d89c99e5526d5ad001f404b9b4c44f613042
Kratos1702 <mespositow@gmail.com>
2023-10-17 21:15:28 +0200

    Fix prodcons logs

--- produttore_consumatore/prodcons_coda_circolare.c
+++ produttore_consumatore/prodcons_coda_circolare.c
@@ -67,7 +67,7 @@ void produttore(struct prodcons * p, int ds_sem) {
 	// genera valore tra 0 e 99
 	p->buffer[p->testa] = rand() % 100;
 
-	printf("Il valore prodotto = %d\n", p->buffer[p->testa]);
+	printf("[PROD %d] Il valore prodotto = %d\n", getpid(), p->buffer[p->testa]);
 
 	p->testa = (p->testa+1) % DIM_BUFFER;
 
@@ -88,7 +88,7 @@ void consumatore(struct prodcons * p, int ds_sem) {
 
 	sleep(2);
 
-	printf("Il valore consumato = %d\n", p->buffer[p->coda]);
+	printf("[CONS %d] Il valore consumato = %d\n", getpid(), p->buffer[p->coda]);
 
 	p->coda = (p->coda + 1) % DIM_BUFFER;
 
@@ -131,7 +131,7 @@ int main() {
 
 
 
-	for(int i=0; i<NUM_CONSUMATORI; i++) {
+	for(int i=0; i<NUM_CONSUMATORI; ++i) {
 
 		int pid = fork();
 
@@ -148,6 +148,7 @@ int main() {
 
 			consumatore(p, ds_sem);
 
+			printf("Figlio consumatore terminato: %d\n", getpid());
 			exit(1);
 		}
 	}
@@ -155,7 +156,7 @@ int main() {
 
 
 
-	for(int i=0; i<NUM_PRODUTTORI; i++) {
+	for(int i=0; i<NUM_PRODUTTORI; ++i) {
 
 		int pid = fork();
 
@@ -171,21 +172,16 @@ int main() {
 			srand(getpid()*time(NULL));
 
 			produttore(p, ds_sem);
-
+			
+			printf("Figlio produttore terminato: %d\n", getpid());
 			exit(1);
 		}
 	}
 
 
 
-	for(int i=0; i<NUM_PRODUTTORI; i++) {
+	for(int i=0; i< (NUM_PRODUTTORI + NUM_CONSUMATORI); ++i) {
 		wait(NULL);
-		printf("Figlio produttore terminato\n");
-	}
-
-	for(int i=0; i<NUM_CONSUMATORI; i++) {
-		wait(NULL);
-		printf("Figlio consumatore terminato\n");
 	}
 
 
